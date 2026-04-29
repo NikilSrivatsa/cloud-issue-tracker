e80e24fa3bdfa35e609b441e16d4b2bedbacb929
NikilSrivatsa <iam.nikil97@gmail.com>
2026-04-29 22:17:25 +0530

    Delete terraform/outputs.tf

--- terraform/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "public_ip" {
-  description = "Public IP address of the deployed application."
-  value       = aws_instance.app.public_ip
-}
-
-output "application_url" {
-  description = "HTTP URL of the deployed application."
-  value       = "http://${aws_instance.app.public_ip}"
-}
